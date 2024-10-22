a75de934c1725ab1c7b1821ba56197482935d7bc
Bhavesh Tiwari <tiwaribhavesh45@gmail.com>
2024-10-22 10:10:48 +0000

    cors err resolved

--- srv/server.js
+++ srv/server.js
@@ -2,11 +2,6 @@ const cds = require("@sap/cds");
 const cors = require('cors');
 const cov2ap = require("@cap-js-community/odata-v2-adapter");
 cds.on("bootstrap", function (app) {
-    app.use(cov2ap());
-    app.use((req, res,next) => {
-        res.setHeader('Access-Control-Allow-Origin', 'https://vsdtechno.com');
-        next();
-    });
-    // app.use(cors());
+    app.use([cors(), cov2ap()]);
 });
 module.exports = cds.server;
